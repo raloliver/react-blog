1ab88e18fafe7195f5884ad8dfa91a267bd138ae
raloliver <contato@raloliver.com.br>
2021-08-28 08:14:16 +0100

    Add react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+
+import { BrowserRouter as Router } from "react-router-dom"
+
+import Routes from "Routes"
+
 function App() {
   return (
     <div className="App">
       <h3>Hello React... again!</h3>
+      <Router>
+        <Routes />
+      </Router>
     </div>
   );
 }
 
-export default App;
+export default App

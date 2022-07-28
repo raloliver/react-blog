0df86aa7f63f0a6a5f0e3049b38daacf7bafd3c4
raloliver <contato@raloliver.com.br>
2022-07-28 14:13:51 +0100

    useLocalStorage a custom hook

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+/*
+ * File: App.js
+ * Project: react-blog
+ * Created: Tuesday, May 3rd 2022, 3:27:15 pm
+ * Last Modified: Thursday, July 28th 2022, 2:04:42 pm
+ * Copyright © 2022 AMDE Agência
+ */
+
 import {BrowserRouter as Router} from 'react-router-dom'
 
 import Routes from 'Routes'

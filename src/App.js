af995fc9f1fd6c55e3b2928bab38d03e22bb5bef
raloliver <contato@raloliver.com.br>
2022-07-28 15:05:46 +0100

    3.20 current user context

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
  * File: App.js
  * Project: react-blog
  * Created: Tuesday, May 3rd 2022, 3:27:15 pm
- * Last Modified: Thursday, July 28th 2022, 2:04:42 pm
+ * Last Modified: Thursday, July 28th 2022, 2:55:45 pm
  * Copyright © 2022 AMDE Agência
  */
 
@@ -10,13 +10,16 @@ import {BrowserRouter as Router} from 'react-router-dom'
 
 import Routes from 'Routes'
 import Topbar from 'components/Topbar'
+import {CurrentUserProvider} from 'contexts/currentUser'
 
 function App() {
   return (
-    <Router>
-      <Topbar />
-      <Routes />
-    </Router>
+    <CurrentUserProvider>
+      <Router>
+        <Topbar />
+        <Routes />
+      </Router>
+    </CurrentUserProvider>
   )
 }
 

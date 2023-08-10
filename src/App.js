10dab28472126769201cd28be6f59ffba3ea1f0e
raloliver <raloliver.ws@gmail.com>
2023-08-10 05:18:00 +0100

    323/324: fetching current user data and use callback hook

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
  * File: App.js
  * Project: react-blog
  * Created: Tuesday, May 3rd 2022, 3:27:15 pm
- * Last Modified: Thursday, July 28th 2022, 2:55:45 pm
+ * Last Modified: Thursday, August 10th 2023, 5:03:15 am
  * Copyright © 2022 AMDE Agência
  */
 
@@ -11,14 +11,17 @@ import {BrowserRouter as Router} from 'react-router-dom'
 import Routes from 'Routes'
 import Topbar from 'components/Topbar'
 import {CurrentUserProvider} from 'contexts/currentUser'
+import UserChecker from 'components/UserChecker'
 
 function App() {
   return (
     <CurrentUserProvider>
-      <Router>
-        <Topbar />
-        <Routes />
-      </Router>
+      <UserChecker>
+        <Router>
+          <Topbar />
+          <Routes />
+        </Router>
+      </UserChecker>
     </CurrentUserProvider>
   )
 }
